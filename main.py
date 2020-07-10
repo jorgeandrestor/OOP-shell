0346715c0ac4ea4e3db09c9675987bfc9630298c
Jorge <jorgeandrestor@gmail.com>
2020-07-09 22:50:34 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from userFile import User
+
+###CREATE SOME USERS
+
+user1 = User("Ty", "Tdog@aol.com", 16, "Pa$$word")
+user2 = User("Sarai", "SMendes@hotmail.com", 15, "Kangaroo!")
+user3 = User("Osu", "Osu22@gmail.com", 12, "12345abcde")
+
+
+
+
+###CHECK A USER"S INFO
+print(user2.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#WHY THIS OVER DICTIONARIES?
+# user1 = {"name": "Ty", "email": "Tdog@aol.com", "age":16, "password": "Pa$$word"}
+# user2 = {"name": "Sarai", "email": "SMenders@hotmail.com", "age":15, "password": "Kangaroo!}
+# user3 = {"name": "Osu", "email": "Osu22@gmail.com", "age":12, "password": "12345abcde"}
